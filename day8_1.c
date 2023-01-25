3f2b4e47cb93edacad6ecb481494d0076f61a4d2
abhilathiya4 <123169803+abhilathiya4@users.noreply.github.com>
2023-01-25 17:18:55 +0530

    Add files via upload

---
+++ day8_1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main(){
+	
+	int a,b;
+	
+	printf("enter value A : ");
+	scanf("%d",&a);
+
+	printf("enter value B : ");
+	scanf("%d",&b);
+	if(a<b){
+		printf("A is min \n");
+		printf("B is max");	}
+	
+	else{
+		printf("A is max \n");
+		printf("B is min");	}
+	
+	
+	
+	
+}
---
+++ day8_2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+main(){
+	
+	int a;
+	
+	printf("ENTER A NUMBER : ");
+	scanf("%d",&a);
+	
+	if (a==0){
+		
+		printf("NETURAL");
+		
+	}
+	
+	else if (a>0){
+		printf("POSITIVE");	}	
+	
+	else {
+		
+		printf("NEGATIVE");
+			}
+	
+}
